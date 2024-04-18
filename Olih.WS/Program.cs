20c8056c4c9cdc0daccfa7cdaa34e1defb8ee112
Kendy - CMC Global <thanhnhan.lionus@gmail.com>
2024-04-18 22:05:41 +0700

    remove worker app

--- Olih.WS/Program.cs
+++
@@ -1,7 +0,0 @@
-using Olih.WS;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
--- README.md
+++ README.md
@@ -4,5 +4,3 @@ API Client library for SAP Data Inteface using Adapter pattern
 - dotNet 8.0
 - SAP 9~10
 - Rest, Json, Web, Api
-
--
